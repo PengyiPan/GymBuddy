bb438ec207e6b3c2dcf8b425eea06d481ee68068
Pengyi Pan <nickpanmail@gmail.com>
2014-11-11 22:05:42 -0500

    added custom query php connection

--- GymBuddy/MatchingViewController.swift
+++ GymBuddy/MatchingViewController.swift
@@ -19,7 +19,7 @@ class MatchingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        textView.text = (receivedQueryTime + "\n" + receivedQueryLocation + "\n" + receivedQuerySport + "\n" + receivedQueryCategory)
+        makeDatabaseQuery()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,6 +29,26 @@ class MatchingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func makeDatabaseQuery() {
+        var bodyData = "query= SELECT * FROM PostedWorkOutRecord"
+        
+        
+        let URL: NSURL = NSURL(string: "http://pengyipan.com/service.php")!
+        let request:NSMutableURLRequest = NSMutableURLRequest(URL:URL)
+        request.HTTPMethod = "POST"
+        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding);
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue())
+            {
+                (response, data, error) in
+                var output = NSString(data: data, encoding: NSUTF8StringEncoding) // new output variable
+                println(output!)
+                self.textView.text = output!
+                //TODO: parse received Json data
+                
+        }
+    }
+    
+  
     
 }
 
