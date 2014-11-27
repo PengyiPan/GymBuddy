035b3347d2ab8e733070e1235fc0dab3a64b5cf4
JustinLovesCompsci <justinzihaozhang@gmail.com>
2014-11-27 18:58:24 -0500

    add profile view controller

---
+++ GymBuddy/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  GymBuddy
+//
+//  Created by Justin (Zihao) Zhang on 11/27/14.
+//  Copyright (c) 2014 Duke 2014 Fall CS316. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
