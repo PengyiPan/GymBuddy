683930373762a371ee0b4c40002c299b5bc092b5
JustinLovesCompsci <justinzihaozhang@gmail.com>
2014-11-27 00:55:50 -0500

    add code to logInViewController

--- GymBuddy/LogInViewController.swift
+++ GymBuddy/LogInViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class LogInViewController: UIViewController {
     
+    @IBOutlet weak var netIDTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBAction func logInButton(sender: AnyObject) {
+        var netID = netIDTextField.text
+        var password = passwordTextField.text
+        if !netID.isEmpty && !password.isEmpty {
+            //TODO: send to database for verification
+        } else {
+            popUpAlertDialog("Alert", message: "Fill all the fields", buttonText: "OK")
+        }
+    }
+    
+    func popUpAlertDialog(alert:String, message:String, buttonText:String){
+        var alert = UIAlertController(title: alert, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: buttonText, style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
