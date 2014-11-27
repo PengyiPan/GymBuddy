41ae182901511cd03bfc2bbe856c94e5501770f2
JustinLovesCompsci <justinzihaozhang@gmail.com>
2014-11-27 00:29:16 -0500

    finish registration GUI

--- GymBuddy/RegistrationViewController.swift
+++ GymBuddy/RegistrationViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
     
+    @IBOutlet weak var TitleTextField: UILabel!
+    @IBOutlet weak var netIDTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var rePasswordTextField: UITextField!
+    
+    @IBAction func RegisterButton(sender: AnyObject) {
+        if !netIDTextField.text.isEmpty && !passwordTextField.text.isEmpty && !rePasswordTextField.text.isEmpty {
+            if(passwordTextField.text != rePasswordTextField.text){
+                popUpAlertDialog("Alert", message: "Password not matched", buttonText: "OK")
+                return;
+            }
+            //send registration info to database
+            
+        } else {
+            popUpAlertDialog("Alert", message: "Fill all the fields", buttonText: "OK")
+        }
+    }
+    
+    func popUpAlertDialog(alert:String, message:String, buttonText:String){
+        var alert = UIAlertController(title: alert, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: buttonText, style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
