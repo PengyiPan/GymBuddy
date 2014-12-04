8c902487e3db4ff63cfc10a79db3011db7aca171
JustinLovesCompsci <justinzihaozhang@gmail.com>
2014-12-04 13:10:37 -0500

    update README' gi

---
+++ GymBuddy/ProfileUpdateModel.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileUpdateModel.swift
+//  GymBuddy
+//
+//  Created by Justin (Zihao) Zhang on 12/4/14.
+//  Copyright (c) 2014 Duke 2014 Fall CS316. All rights reserved.
+//
+
+import Foundation
